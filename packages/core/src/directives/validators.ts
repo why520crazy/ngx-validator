aa75fcb39a064ca3f8b48a95b136a96645002332
why520crazy <why520crazy@163.com>
2019-01-15 20:33:00 +0800

    Feature: add ngxMin and ngxMax directive #1

--- packages/core/src/directives/validators.ts
+++ packages/core/src/directives/validators.ts
@@ -1,50 +1,50 @@
-// import { Directive, forwardRef, Attribute, Injectable, ElementRef, Input } from '@angular/core';
-// import { NG_VALIDATORS, Validator, AbstractControl, FormControl, Validators, ValidatorFn } from '@angular/forms';
+import { Directive, forwardRef, Attribute, Injectable, ElementRef, Input } from '@angular/core';
+import { NG_VALIDATORS, Validator, AbstractControl, FormControl, Validators, ValidatorFn } from '@angular/forms';
 
-// @Directive({
-//     selector: '[ngx-min][formControlName],[ngx-min][formControl],[ngx-min][ngModel],',
-//     providers: [
-//         {
-//             provide: NG_VALIDATORS,
-//             useExisting: forwardRef(() => MinValidatorDirective),
-//             multi: true
-//         }
-//     ]
-// })
-// export class MinValidatorDirective implements Validator {
-//     private validator: ValidatorFn;
+@Directive({
+    selector: 'input[type=number][ngxMin][formControlName],input[type=number][ngxMin][formControl],input[type=number][ngxMin][ngModel],',
+    providers: [
+        {
+            provide: NG_VALIDATORS,
+            useExisting: forwardRef(() => MinValidatorDirective),
+            multi: true
+        }
+    ]
+})
+export class MinValidatorDirective implements Validator {
+    private validator: ValidatorFn;
 
-//     @Input() public set min(value: string) {
-//         this.validator = Validators.min(parseInt(value, 10));
-//     }
+    @Input() public set ngxMin(value: string) {
+        this.validator = Validators.min(parseInt(value, 10));
+    }
 
-//     constructor() {}
+    constructor() {}
 
-//     validate(control: AbstractControl) {
-//         return this.validator(control);
-//     }
-// }
+    validate(control: AbstractControl) {
+        return this.validator(control);
+    }
+}
 
-// @Directive({
-//     selector: 'input[type=number][max][formControlName],input[type=number][max][formControl],input[type=number][max][ngModel]',
-//     providers: [
-//         {
-//             provide: NG_VALIDATORS,
-//             useExisting: forwardRef(() => MaxValidatorDirective),
-//             multi: true
-//         }
-//     ]
-// })
-// export class MaxValidatorDirective implements Validator {
-//     private validator: ValidatorFn;
+@Directive({
+    selector: 'input[type=number][ngxMax][formControlName],input[type=number][ngxMax][formControl],input[type=number][ngxMax][ngModel]',
+    providers: [
+        {
+            provide: NG_VALIDATORS,
+            useExisting: forwardRef(() => MaxValidatorDirective),
+            multi: true
+        }
+    ]
+})
+export class MaxValidatorDirective implements Validator {
+    private validator: ValidatorFn;
 
-//     @Input() public set max(value: string) {
-//         this.validator = Validators.max(parseInt(value, 10));
-//     }
+    @Input() public set ngxMax(value: string) {
+        this.validator = Validators.max(parseInt(value, 10));
+    }
 
-//     constructor() {}
+    constructor() {}
 
-//     validate(control: AbstractControl) {
-//         return this.validator(control);
-//     }
-// }
+    validate(control: AbstractControl) {
+        return this.validator(control);
+    }
+}
--- packages/core/src/module.ts
+++ packages/core/src/module.ts
@@ -2,11 +2,11 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgxFormValidatorDirective } from './directives/form-validator.directive';
 import { NgxFormSubmitDirective } from './directives/form-submit.directive';
-// import { MaxValidatorDirective, MinValidatorDirective } from './directives/validators';
+import { MaxValidatorDirective, MinValidatorDirective } from './directives/validators';
 
 import { NgxValidatorGlobalConfig, NGX_VALIDATOR_CONFIG } from './validator.class';
 
-const declarations = [NgxFormValidatorDirective, NgxFormSubmitDirective];
+const declarations = [NgxFormValidatorDirective, NgxFormSubmitDirective, MaxValidatorDirective, MinValidatorDirective];
 
 @NgModule({
     declarations: declarations,
