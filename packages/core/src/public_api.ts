1ac47d8d4e4f5de57ef24aee231226742eff6c80
why520crazy <why520crazy@163.com>
2020-06-10 09:34:28 +0800

    fix: export all validator directives

--- packages/core/src/public_api.ts
+++ packages/core/src/public_api.ts
@@ -6,6 +6,7 @@ export * from './module';
 export * from './validator-loader.service';
 export * from './validator.class';
 export * from './directives/form-validator.directive';
+export * from './directives/validators';
 export * from './directives/form-submit.directive';
 export * from './strategies';
 export { NgxValidators } from './validators';
